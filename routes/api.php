01252b827268e86108bcafa74de4cd8dea2a5cbb
Diego336s <sanabriadiego336@gmail.com>
2025-08-29 20:30:05 -0500

    volvi a subir las rutas

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get("listarClientes", [clientes::class, "index"]);
+Route::post("registrarCliente", [clientes::class, "store"]);
+Route::put("actualizarCliente/{id}", [clientes::class, "update"]);
+Route::delete("eliminarCliente/{id}", [clientes::class, "destroy"]);
+Route::put("cambiarClave/{id}", [clientes::class, "cambiarClave"]);
