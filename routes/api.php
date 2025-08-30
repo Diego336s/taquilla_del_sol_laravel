704421e73411349093914fdfb735bdfe1fdc1013
Oscar30 <oxcarbohorfo13@gmail.com>
2025-08-29 21:06:53 -0500

    Api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Models\clientes;
+use App\Http\Controllers\AdministradoresController;
+use App\Http\Controllers\ClientesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,8 +9,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get("listarClientes", [clientes::class, "index"]);
-Route::post("registrarCliente", [clientes::class, "store"]);
-Route::put("actualizarCliente/{id}", [clientes::class, "update"]);
-Route::delete("eliminarCliente/{id}", [clientes::class, "destroy"]);
-Route::put("cambiarClave/{id}", [clientes::class, "cambiarClave"]);
+Route::get("listarClientes", [ClientesController::class, "index"]);
+Route::post("registrarCliente", [ClientesController::class, "store"]);
+Route::put("actualizarCliente/{id}", [ClientesController::class, "update"]);
+Route::delete("eliminarCliente/{id}", [ClientesController::class, "destroy"]);
+Route::put("cambiarClave/{id}", [ClientesController::class, "cambiarClave"]);
+
+Route::get("listarAdministradores", [AdministradoresController::class, "index"]);
+Route::post("registrarAdministradores", [AdministradoresController::class, "store"]);
+Route::put("actualizarAdministradores/{id}", [AdministradoresController::class, "update"]);
+Route::put("cambiarClave/{id}", [AdministradoresController::class, "cambiarClave"]);
