e8d386bc13615a6e0fb93db8f95043202817b09a
Oscar30 <oxcarbohorfo13@gmail.com>
2025-08-29 20:24:31 -0500

    Controller Administrador

--- app/Http/Controllers/AdministradoresController.php
+++ app/Http/Controllers/AdministradoresController.php
@@ -2,9 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Administradores;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AdministradoresController extends Controller
 {
-    //
+    public function index()
+    {
+        $administradores = Administradores::all();
+        return response()->json($administradores);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nombres' => 'required|string|max:255',
+            'apellidos' => 'required|string|max:255',
+            'documento' => 'required|integer',
+            'telefono' => 'required|integer|min:10',
+            'correo' => 'required|string|max:255',
+            'clave' => 'required|string|min:6',
+            'fecha_nacimiento' => 'required|date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $validatedData['clave'] = Hash::make($validatedData['clave']);
+
+        $administradores = Administradores::create($validatedData);
+
+        return response()->json($administradores, 201);
+    }
+
+    public function show(string $id)
+    {
+        $administradores = Administradores::find($id);
+        if (!$administradores) {
+            return response()->json(['message' => 'Administrador no encontrado'], 404);
+        }
+        return response()->json($administradores);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $administradores = Administradores::find($id);
+        if (!$administradores) {
+            return response()->json(['message' => 'Administrador no encontrado'], 404);
+        }
+
+        $validator = Validator::make($request->all(),[
+            'nombres' => 'sometimes|required|string|max:255',
+            'apellidos' => 'sometimes|required|string|max:255',
+            'documento' => 'sometimes|required|integer',
+            'telefono' => 'sometimes|required|integer|min:10',
+            'correo' => 'sometimes|required|string|max:255',
+            'fecha_nacimiento' => 'sometimes|required|date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $administradores->update($validator->validate());
+
+        return response()->json($administradores);
+    }
+
+    public function cambiarClave(Request $request, string $id){
+        $administradores = Administradores::find($id);
+        if (!$administradores) {
+            return response()->json(['message' => 'Administrador no encontrado'], 404);
+        }
+
+        $validatedData = $request->validate([
+            'clave' => 'required|string|min:6',
+        ]);
+
+        $administradores->clave = Hash::make($validatedData['clave']);
+        $administradores->save();
+        return response()->json($administradores);
+    }
+
+    public function destroy(string $id){
+        $administradores = Administradores::find($id);
+        if (!$administradores) {
+            return response()->json(['message' => 'Administrador no encontrado']);
+        }
+
+        $administradores->delete();
+        return response()->json(['message' => 'Administrador eliminado correctamente']);
+    }
+
 }
