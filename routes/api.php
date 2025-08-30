722a672b587d4a8fdafaba22dec44e003b04be5e
Diego336s <sanabriadiego336@gmail.com>
2025-08-29 20:18:18 -0500

    listar, eliminar, editar, agregar, cambiarClave, del cliente echo en controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Models\clientes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get("listarClientes", [clientes::class, "index"]);
+Route::post("registrarCliente", [clientes::class, "store"]);
+Route::put("actualizarCliente/{id}", [clientes::class, "update"]);
+Route::delete("eliminarCliente/{id}", [clientes::class, "destroy"]);
+Route::put("cambiarClave/{id}", [clientes::class, "cambiarClave"]);
