58bf52c45c4f9f1a53df1fb347fc23b27f1eb992
Oscar30 <oxcarbohorfo13@gmail.com>
2025-09-12 21:35:40 -0500

    Migracion Eventos

--- database/migrations/2025_08_30_001034_create_eventos_table.php
+++ database/migrations/2025_08_30_001034_create_eventos_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('eventos', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->text('descripcion')->nullable();
+            $table->date('fecha_inicio');
+            $table->date('fecha_fin');
+            $table->string('lugar');
+            $table->integer('capacidad');
+            $table->enum('estado', ['activo', 'inactivo']);
+            $table->foreignId('empresa_id')->constrained('empresa', 'id');
+            $table->foreignId('categoria_id')->constrained('categorias', 'id');
             $table->timestamps();
         });
     }
