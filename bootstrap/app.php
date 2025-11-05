c6919514932be54ff684d3488764af55c278b741
Diego336s <sanabriadiego336@gmail.com>
2025-11-04 21:29:52 -0500

    Automatizacion del borrar codigos de verificacion

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -8,13 +9,20 @@ use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+
+
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('app:borrar-codigos-expirados')->everyMinute();
+    })
+
+
     ->withMiddleware(function (Middleware $middleware): void {
-       $middleware->alias([
+        $middleware->alias([
             'abilities' => CheckAbilities::class,
             'ability' => CheckForAnyAbility::class,
         ]);
